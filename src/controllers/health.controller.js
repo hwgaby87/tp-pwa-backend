6ce94b80881483f2d5cc7a1955fce01f070873dd
gabriela <hwgaby87@gmail.com>
2026-04-20 19:05:16 -0300

    Health Módulo

---
+++ src/controllers/health.controller.js
@@ -0,0 +1,34 @@
+import userRepository from "../repository/user.repository.js"
+
+class HealthController {
+    getApi (request, response) {
+        response.status(200).json(
+            {
+                message: "La API funciona correctamente",
+                status: 200,
+                ok: true
+            }
+        )
+    }
+
+    async getDB (request, response, next){
+        try{
+            await userRepository.getUser()
+            return response.status(200).json(
+                {
+                    message: "La DB funciona correctamente",
+                    status: 200,
+                    ok: true
+                }
+            )
+        }
+        catch(error){
+            next(error)
+        }
+    }
+
+}
+
+const healthController = new HealthController()
+
+export default healthController
